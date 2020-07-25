6ea52e781da35dd7a82dd708d7eb0ca29bb9ee5e
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2020-07-26 00:44:18 +0530

    Add files via upload
    Added the code for main menu buttons.

--- Classes/MainMenu.cpp
+++ Classes/MainMenu.cpp
@@ -46,19 +46,43 @@ bool MainMenu::init() {
     this->addChild(menu, 1);
 
     // Background Sprite
-    auto backgroundSprite = Sprite::create("Background.png");
+    auto backgroundSprite = Sprite::create("background/city11.png");
+    //auto backgroundSprite = Sprite::create("city1.png");
     backgroundSprite->setPosition(origin.x + visibleSize.width / 2,
                                   origin.y + visibleSize.height / 2);
     this->addChild(backgroundSprite, 0);
 
-    // Play menu
-    auto playItem = MenuItemImage::create( "Play.png", "CloseNormal.png", CC_CALLBACK_1( MainMenu::GoToLevelSetter, this ) );
-    playItem->setPosition( Vec2( visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y ) );
+    // Game label
+    auto label = Label::createWithTTF("Haunted City", "fonts/VerminVibesV-Zlg3.ttf", 30);
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height -50));
+    label->setTextColor(Color4B::WHITE);
+    //label->enableOutline(Color4B::BLACK);
+    this->addChild(label, 2);
 
+    // Play menu
+    auto playItem = MenuItemImage::create( "play-button.png", "CloseNormal.png", CC_CALLBACK_1( MainMenu::GoToLevelSetter, this ) );
+    playItem->setPosition( Vec2( visibleSize.width / 2 + origin.x + 83, visibleSize.height / 6 + origin.y + 10) );
+    playItem->setScale(1.5);
     auto menuPlay = Menu::create( playItem, NULL );
     menuPlay->setPosition( Point::ZERO );
-
     this->addChild( menuPlay, 2 );
+
+    // Options menu
+    auto playItem1 = MenuItemImage::create( "options-button.png", "CloseNormal.png", CC_CALLBACK_1( MainMenu::GoToLevelSetter, this ) );
+    playItem1->setPosition( Vec2( visibleSize.width / 2 + origin.x + 60, visibleSize.height / 8 + origin.y ) );
+    playItem1->setScale(1.5);
+    auto menuPlay1 = Menu::create( playItem1, NULL );
+    menuPlay1->setPosition( Point::ZERO );
+    this->addChild( menuPlay1, 2 );
+
+    // Quit
+    auto playItem2 = MenuItemImage::create( "quit-button.png", "CloseNormal.png", CC_CALLBACK_1( MainMenu::menuCloseCallback, this ) );
+    playItem2->setPosition( Vec2( visibleSize.width / 2 + origin.x + 15, visibleSize.height / 10 + origin.y ) );
+    playItem2->setScale(1.5);
+    auto menuPlay2 = Menu::create( playItem2, NULL );
+    menuPlay2->setPosition( Point::ZERO );
+    this->addChild( menuPlay2, 2 );
 
     return true;
 }
